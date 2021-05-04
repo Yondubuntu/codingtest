9211298d9790477d4496bba997cc3049f901c356
yondubuntu <endyd4@yonsei.ac.kr>
2021-05-04 17:54:23 +0900

    commit

---
+++ 시간1.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+count  = 0 
+
+for i in rang (N+1) :
+    for j in range (60) :
+        for z  in range (60) : 
+            if '3' str (i) + str (z) + str(j):
+                count + = 1
+                
